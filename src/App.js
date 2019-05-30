412b30c4b727ce7af762d6d5bd2f34c533c0a401
Mihail Petrov <mihail.petrov@isobar.com>
2019-05-30 15:19:12 +0300

    +layout component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Layout from './components/Layout'
 
 function App() {
   return (
     <div className="App">
-
+      <Layout>
+        Content placeholder
+      </Layout>
     </div>
   );
 }
